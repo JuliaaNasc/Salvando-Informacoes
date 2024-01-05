e3729e10cfea74690d5c15322a6f17e5e70aa54b
Júlia Nascimento <juliavcostan@gmail.com>
2024-01-05 18:27:12 -0300

    Ajuste de funções

--- lib/pages/main_screen.dart
+++ lib/pages/main_screen.dart
@@ -19,108 +19,115 @@ class _MainScreenState extends State<MainScreen> {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset('assets/wave/wave_purple_total.png'),
-            Expanded(child: SizedBox()),
-            SizedBox(
-              height: 150,
-              child: Lottie.asset('assets/animations/animation_cat_02.json'),
-            ),
-            SizedBox(height: 10),
-            const Text(
-              'PREENCHA O CAMPO ABAIXO: ',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/wave/wave_purple_total.png'),
+              SizedBox(
+                height: 150,
+                child: Lottie.asset('assets/animations/animation_cat_02.json'),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(20, 10, 20, 25),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Color.fromARGB(255, 221, 201, 201),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextField(controller: resultado1),
-                ),
-              ),
-            ),
-            const Text(
-              'PREENCHA O CAMPO ABAIXO: ',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Color.fromARGB(255, 221, 201, 201),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextField(controller: resultado2),
-                ),
-              ),
-            ),
-            SizedBox(height: 15),
-            ElevatedButton(
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.purple),
-              ),
-              onPressed: () {
-                setState(
-                  () {
-                    if (resultado1.text != '' && resultado2.text.isNotEmpty) {
-                      texto =
-                          'Foi adquirido as informações ${resultado1.text.toUpperCase()} e ${resultado2.text.toUpperCase()}';
-                    } else {
-                      mensagem = 'NENHUM TEXTO ENCONTRADO';
-                    }
-                  },
-                );
-              },
-              child: const Text(
-                ' HELLO WORLD . . .',
+              SizedBox(height: 10),
+              const Text(
+                'PREENCHA O CAMPO ABAIXO: ',
                 style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-            ),
-            Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(5, 0, 5, 0),
-                  child: Text(
-                    texto,
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                    ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 10, 20, 25),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 221, 201, 201),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextField(controller: resultado1),
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(5, 0, 5, 0),
-                  child: Text(
-                    mensagem,
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(
-                      color: Colors.red,
-                      fontWeight: FontWeight.bold,
-                    ),
+              ),
+              const Text(
+                'PREENCHA O CAMPO ABAIXO: ',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 221, 201, 201),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextField(controller: resultado2),
                   ),
                 ),
-              ],
-            ),
-            Expanded(child: SizedBox()),
-            Image.asset('assets/wave/wave_purple_total.png'),
-          ],
+              ),
+              SizedBox(height: 15),
+              ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.purple),
+                ),
+                onPressed: () {
+                  setState(
+                    () {
+                      if (resultado1.text != '' && resultado2.text.isNotEmpty) {
+                        texto =
+                            'Foi adquirido as informações ${resultado1.text.toUpperCase()} e ${resultado2.text.toUpperCase()}';
+                        mensagem = '';
+                      } else {
+                        mensagem = 'NENHUM TEXTO ENCONTRADO';
+                        texto = '';
+                      }
+                    },
+                  );
+                },
+                child: const Text(
+                  ' HELLO WORLD . . .',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Column(
+                children: [
+                  if (texto != '')
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(5, 10, 5, 0),
+                    child: Text(
+                      texto,
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                   if(mensagem != '')
+                  Padding(
+                   
+                    padding: const EdgeInsets.fromLTRB(5, 10, 5, 0),
+                    child: Text(
+                      mensagem,
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                        color: Colors.red,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Image.asset('assets/wave/wave_purple_total.png'),
+            ],
+          ),
         ),
       ),
     );
